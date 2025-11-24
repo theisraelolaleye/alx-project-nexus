40c31b9d104fd6005124d057a114fc0de845787f
Belove <beloveolusola@gmail.com>
2025-11-24 03:57:38 -0700

    feat: Sample data population

--- jobfrica_backend/build.sh
+++ jobfrica_backend/build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-echo "Installing dependencies..."
+set -o pipefail
+set -o nounset
 pip install -r requirements.txt
 
-echo "Collecting static files..."
+
 python manage.py collectstatic --no-input
 
-echo "Running migrations..."
 python manage.py migrate
 
 echo "Build complete!"
